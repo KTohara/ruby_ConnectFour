715b34eedd33415c3216f746fe7c5979e0082fb7
kenta <ktohara@gmail.com>
2022-07-10 20:09:35 -0400

    Finished #diagonal (left & right), #four_in_a_row

--- lib/board.rb
+++ lib/board.rb
@@ -1,39 +1,79 @@
+# frozen_string_literal: true
 require 'byebug'
 
 class Board
-  attr_reader :row, :col, :board
+  attr_reader :rows, :cols, :grid
 
   def initialize
-    @row = 6
-    @col = 7
-    @board = Array.new(@row) { [''] * @col }
+    @rows = 6
+    @cols = 7
+    @grid = Array.new(@rows) { [''] * @cols }
   end
 
   def game_over?
-    return true if win? || draw?
-    false
+    win? || draw?
   end
 
   def win?
-    diag_connect? || row_connect? || col_connect?
+    cols = grid.transpose
+    diag = diagonal_left(grid)
+
+    four_in_a_row?(grid) || four_in_a_row?(cols) || four_in_a_row?(diag)
+    # diag_four? || four_in_a_row?(matrix) || col_four?
   end
 
-  def draw?
+  def draw?; end
 
+  def diagonal_left(matrix)
+    lower = (0...rows - 3).map do |i|
+      (0...rows - i).map do |j|
+        matrix[i + j][j]
+      end
+    end
+    
+    upper = (1...cols- 3).map do |i|
+      (0...rows - i + 1).map do |j|
+        matrix[j][i + j]
+      end
+    end
+
+    lower + upper
   end
 
-  def diag_connect?
+  def diagonal_right(matrix)
+    upper = (4..rows).map do |i|
+      (0...i).map do |j|
+        matrix[i - j - 1][j]
+      end
+    end
 
+    lower = (0...cols- 4).map do |i|
+      (i...rows).map do |j|
+        matrix[j][i - j - 1]
+      end
+    end
+
+    upper + lower
   end
 
-  def row_connect?
-
-  end
-
-  def col_connect?
-
+  def four_in_a_row?(matrix)
+    matrix.each do |rows|
+      rows.each_cons(4).each do |fours|
+        return true if fours.uniq.size == 1 && !fours.first.empty?
+      end
+    end
+    false
   end
 end
 
-# b = Board.new
-# p b.board.all? { |el| el.empty? }
+b = Board.new
+grid = [
+[ 0,  1,  2,  3,  4,  5,  6],
+[ 7,  8,  9, 10, 11, 12, 13], 
+[14, 15, 16, 17, 18, 19, 20], 
+[21, 22, 23, 24, 25, 26, 27], 
+[28, 29, 30, 31, 32, 33, 34], 
+[35, 36, 37, 38, 39, 40, 41]]
+
+p b.diagonal_left(grid)
+p b.diagonal_right(grid)
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,16 +1,20 @@
+# frozen_string_literal: true
+
 require 'board'
 
 describe Board do
   subject(:board) { described_class.new }
+  let(:p1) { :black }
+  let(:p2) { :red }
 
   describe '#initialize' do
     it 'creates a nested array with 42 elements' do
-      board_size = subject.board.flatten.length
+      board_size = subject.grid.flatten.length
       expect(board_size).to eq(42)
     end
 
     it 'should all be empty strings' do
-      board_cells = subject.board.flatten
+      board_cells = subject.grid.flatten
       expect(board_cells).to all(be_empty)
       expect(board_cells).to all(be_a(String))
     end
@@ -26,6 +30,7 @@ describe Board do
 
     context 'when game is a #draw?' do
       it 'returns true' do
+        allow(board).to receive(:win?).and_return(false)
         allow(board).to receive(:draw?).and_return(true)
         expect(board).to be_game_over
       end
@@ -41,43 +46,112 @@ describe Board do
   end
 
   describe '#win?' do
-    context 'when there is a diagonal connect four' do
+    context 'when there is a row four-in-a-row' do
       it 'returns true' do
-        allow(board).to receive(:diag_connect?).and_return(true)
+        allow(board).to receive(:four_in_a_row?).once.and_return(true)
         expect(board).to be_win
       end
     end
 
-    context 'when there is a row connect four' do
+    context 'when there is a column four-in-a-row' do
       it 'returns true' do
-        allow(board).to receive(:row_connect?).and_return(true)
+        allow(board).to receive(:four_in_a_row?).twice.and_return(false, true)
         expect(board).to be_win
       end
     end
 
-    context 'when there is a collumn connect four' do
+    context 'when there is a diagonal four-in-a-row' do
       it 'returns true' do
-        allow(board).to receive(:col_connect?).and_return(true)
+        allow(board).to receive(:four_in_a_row?).at_least(3).times.and_return(false, false, true)
         expect(board).to be_win
       end
     end
 
-    context 'when there is no #connect_four?' do
+    context 'when there is no four-in-a-row' do
       it 'returns false' do
-        allow(board).to receive(:diag_connect?).and_return(false)
-        allow(board).to receive(:row_connect?).and_return(false)
-        allow(board).to receive(:col_connect?).and_return(false)
+        allow(board).to receive(:four_in_a_row?).exactly(3).times.and_return(false, false, false)
         expect(board).to_not be_win
       end
     end
   end
 
-  describe '#diag_connect?' do
-    context 'when there is a diagonal connect four' do
-      before {}
-      it 'returns true' do
+  describe '#four_in_a_row?' do
+    context 'when there is a row with four-in-a-row' do
+      let(:grid) { [
+        ['', '', '', p1, p1, p1, p1],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '']
+      ] }
 
+      it 'returns true' do
+        expect(board.four_in_a_row?(grid)).to be true
       end
+    end
+
+    context 'when there is NO row with four-in-a-row' do
+      let(:grid) { [
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', ''],
+        ['', '', '', '', '', '', '']
+      ] }
+
+      it 'returns false' do
+        expect(board.four_in_a_row?(grid)).to_not be true
+      end
+    end
+  end
+
+  describe '#diagonal_left' do
+    let(:grid) { [
+      ['', 'x', '', 'y', '', '', ''],
+      ['o', p1, 'x', p1, 'y', '', ''],
+      ['', 'o', p1, 'x', p1, 'y', ''],
+      ['', 'z', 'o', p1, '', p1, 'y'],
+      ['', '', 'z', 'o', p1, '', p1],
+      ['', '', '', 'z', '', '', '']
+    ] }
+
+    let(:diagonals) { [
+      ['', 'z', 'z', 'z'],
+      ['o', 'o', 'o', 'o', ''],
+      ['', p1, p1, p1, p1, ''],
+      ['x', 'x', 'x', '', '', ''],
+      ['', p1, p1, p1, p1],
+      ['y', 'y', 'y', 'y']
+    ] }
+
+    it 'returns a nested array with all diagonals from rleft to right' do
+      
+      expect(board.diagonal_left(grid)).to match_array(diagonals)
+    end
+  end
+
+  describe 'diagonal_right' do
+    let(:grid) { [
+      ['', '', '', 'x', '', 'o', ''],
+      ['', '', 'x', p1, 'o', '', 'y'],
+      ['', 'x', p1, 'o', '', 'y', ''],
+      ['x', p1, 'o', '', '', p2, ''],
+      [p1, 'o', 'z', '', p2, '', ''],
+      ['o', 'z', '', '', '', '', '']
+    ] }
+
+    let(:diagonals) { [
+      ['x', 'x', 'x', 'x'],
+      [p1, p1, p1, p1, ''],
+      ['o', 'o', 'o', 'o', 'o', 'o'],
+      ['', '', '', '', 'z', 'z'],
+      ['y', 'y', '', '', ''],
+      ['', p2, p2, '']
+    ] }
+    it 'returns a nested array with diagonals from right to left' do
+      expect(board.diagonal_right(grid)).to match_array(diagonals)
     end
   end
 end
